a1b67ca61edb58020dfccf1b9f7c2a0c5d3e5770
Matu <matiaslucero1717@gmail.com>
2025-01-30 23:04:31 -0300

    subiendo a git

---
+++ js/scriptplatinos.js
@@ -0,0 +1,11 @@
+const platinos = ["Dark Souls: REMASTERED", "Calico", "Pode", "Tomb Raider", "Little Misfortune", "Blaze and the Monster Machines Axle City Racers", "Stumble guys", "Brothers - A Tale Of Two Sons", "Go-go town", "My Coworker Is An Idiot", "Unpacking", "We Were Here: Expeditions", "Assemble With Care", "Transformice", "Gym Simulator 24", "Monument Valley II", "Florence", "Rainy Season", "Samsara Room", "Pineapple On Pizza", "A Building Full Of Cats", "Resonance Of The Ocean"];
+
+
+const lista = document.getElementById("lista_platinos");
+
+platinos.forEach(juego => {
+    const li = document.createElement("li");
+    li.textContent = juego;
+    lista.appendChild(li);
+});
+
