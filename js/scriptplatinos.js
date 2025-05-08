08d26b0edeb63ffe99fc2f46c1a7b05bab0fd76d
Matu <matiaslucero1717@gmail.com>
2025-05-08 02:02:43 -0300

    added one armed cook

--- js/scriptplatinos.js
+++ js/scriptplatinos.js
@@ -1,4 +1,4 @@
-const platinos = ["Dark Souls: REMASTERED", "Resident Evil 4 (2005)", "Calico", "Pode", "Tomb Raider", "Little Misfortune", "Blaze and the Monster Machines Axle City Racers", "Stumble guys", "Brothers - A Tale Of Two Sons", "Go-go town", "My Coworker Is An Idiot", "Unpacking", "We Were Here: Expeditions", "Assemble With Care", "Transformice", "Gym Simulator 24", "Monument Valley II", "Florence", "Rainy Season", "Samsara Room", "Pineapple On Pizza", "A Building Full Of Cats", "Resonance Of The Ocean"];
+const platinos = ["Dark Souls: REMASTERED", "Resident Evil 4 (2005)", "Calico", "Pode", "Tomb Raider", "Little Misfortune", "Blaze and the Monster Machines Axle City Racers", "Stumble guys", "One-armed cook", "Brothers - A Tale Of Two Sons", "Go-go town", "My Coworker Is An Idiot", "Unpacking", "We Were Here: Expeditions", "Assemble With Care", "Transformice", "Gym Simulator 24", "Monument Valley II", "Florence", "Rainy Season", "Samsara Room", "Pineapple On Pizza", "A Building Full Of Cats", "Resonance Of The Ocean"];
 
 
 const lista = document.getElementById("lista_platinos");
